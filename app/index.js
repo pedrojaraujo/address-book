3c2dbe62a772cb4bb7c29b0ebf0344598c207106
pedrojaraujo <pedroa990@gmail.com>
2024-02-05 17:12:59 -0300

    adding the buttons for remove and adding contact

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { Button, FlatList, StyleSheet, Text, View } from "react-native";
 import { useEffect, useState } from "react";
 import { fetchDadosAPI } from "../utils/API";
 
 const Home = () => {
   const [dados, setDados] = useState(null);
+  const [contactosSelecionados, setContactosSelecionados] = useState([]);
+  const [contactosDisponiveis, setContactosDisponiveis] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,10 +20,25 @@ const Home = () => {
     fetchData();
   }, []);
 
+/*   const adicionarContacto = (contacto) => {
+    setContactosSelecionados([...contactosSelecionados, contacto]);
+    setContactosDisponiveis(
+      contactosDisponiveis.filter((c) => c.login.uuid !== contacto.login.uuid)
+    );
+  }; */
+
+  const removerContacto = (contacto) => {
+    setContactosSelecionados(
+      contactosSelecionados.filter((c) => c.login.uuid !== contacto.login.uuid)
+    );
+    setContactosDisponiveis([...contactosDisponiveis, contacto]);
+  };
+
   const renderItem = ({ item }) => (
     <View style={styles.item}>
       <Text>Gender: {item.gender}</Text>
       <Text>Name: {item.name.first} {item.name.last}</Text>
+      <Button title="Remover" onPress={() => removerContacto(item)} />
     </View>
   );
 
@@ -42,16 +59,19 @@ const Home = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 6,
     backgroundColor: "steelblue",
     alignItems: "center",
     justifyContent: "center",
     marginTop: 300,
+    width: "100%",
+    padding: 0,
   },
   item: {
     borderBottomWidth: 1,
     borderBottomColor: "#ccc",
-    padding: 10,
+    paddingVertical: 10,
+    width: "100%",
   },
 });
 
